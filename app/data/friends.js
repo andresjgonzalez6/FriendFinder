00e1d9585f51386bba27fe9ea435f45f3c34b124
andresg6 <ajcapanzana@gmail.com>
2018-11-07 22:22:13 -0700

    wednight

---
+++ app/data/friends.js
@@ -0,0 +1,60 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the reserved tables.
+// Initially we just set it equal to a "dummy" customer.
+// But you could have it be an empty array as well.
+// ===============================================================================
+
+var tableArray = [
+    {
+      customerName: "Ahmed",
+      customerEmail: "ahmed@example.com",
+      customerID: "afhaque89",
+      phoneNumber: "000-000-0000"
+    }
+  ];
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = tableArray;
+  
+
+
+
+  
+// Data needs to be saved in here. 
+
+[
+    {
+    "name": "Ahmed",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+    "scores": [
+    "5",
+    "1",
+    "4",
+    "4",
+    "5",
+    "1",
+    "2",
+    "5",
+    "4",
+    "1"
+    ]
+    },
+    {
+    "name": "Jacob Deming",
+    "photo": "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+    "scores": [
+    "4",
+    "2",
+    "5",
+    "1",
+    "3",
+    "2",
+    "2",
+    "1",
+    "3",
+    "2"
+    ]
+    }
+]
+    
