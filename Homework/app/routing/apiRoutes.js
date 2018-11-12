e3edf9d3eb4e18f0edffa092a0cab8c48073fa54
andresg6 <ajcapanzana@gmail.com>
2018-11-11 17:24:48 -0700

    leavingsbucks

--- Homework/app/routing/apiRoutes.js
+++ Homework/app/routing/apiRoutes.js
@@ -1,28 +1,70 @@
 const friends = require("../data/friends.js");
+const fs = require("fs");
 
 console.log(friends);
 
+function addToFriends(newFriend, friends) {
+    friends.push(newFriend);
 
-function findMatch(friends, newFriend){
+    const data = 'var friends = ' + JSON.stringify(friends, null, 2) + '\n module.exports = friends'
+
+    fs.writeFile('./app/data/friends.js', data, err => {
+        if (err) {
+            console.log(err);
+        }
+    })
+}
+
+function findMatch(friends, newFriend) {
     let scoreComparison = 100;
     let closestMatch;
 
-    friends.forEach(friend =>{
+    friends.forEach(friend => {
         let currentScore = 0;
         for (let i = 0; i < friend.scores.length; i++) {
-                        
+            currentScore += (Math.abs(parseInt(friend.scores[i]) - parseInt(newFriend.answers[i])));
         }
+
+        console.log("Current Score: " + currentScore);
+        if (currentScore < scoreComparison) {
+            scoreComparison = currentScore;
+            closestMatch = friend;
+        }
+
+        return closestMatch;
     });
 }
 
 module.exports = function (app) {
-    app.get("/api/friends", function (req, res) {
-        res.json(friends);
+    app.get('/friends', (req, res) => {
+        fs.readFile('./app/data/friends.js', 'utf-8', (err, data) => {
+            if (err) {
+                console.log(err);
+                res.send('Someting went wrong, please try again');
+            }
+
+            res.send('<code>' + JSON.stringify(friends, undefined, 2) + '</code>');
+        })
     });
 
-    app.post("/api/friends", function (req, res) {
-        friends.push(req.body);
-        // console.log(res.json(friends));
+    app.post('/', (req, res) => {
+
+        req = req.body;
+        const newFriend = {
+            name: req.name,
+            pic: req.pic,
+            answers: []
+        };
+
+        for (answer in req.q) {
+            newFriend.answers.push(req.q[answer]);
+        }
+
+        const match = findMatch(newFriend, friends);
+        addToFriends(newFriend, friends);
+
+        res.jsonp(JSON.stringify(match));
+
     });
 };
 
